319539deedaf888934237c17b85e3418075a1ba4
LilyLiu0719 <lily.ylliu@gmail.com>
2019-12-30 23:30:56 +0800

    fix bclt

--- hw4/SingleCycleMIPS_FPU-2.v
+++ hw4/SingleCycleMIPS_FPU-2.v
@@ -397,8 +397,8 @@ always@(*)begin
 		end
 		6'h31: begin //lwcl
 			OEN_w = 1'b0;
-			CEN_w = 1'b0;
-			WEN_w = 1'b1;
+			CEN_w = 1'b1;
+			WEN_w = 1'b0;
 			A_w = $unsigned( $signed(Freg_r[_fs]) + _immediate) >> 2;
 			Freg_w[_rt] = ReadDataMem;
 			IR_addr_w = IR_addr_r + 32'd4;
@@ -420,16 +420,16 @@ always@(*)begin
 				4'd0: begin
 					process_counter_w = 4'd1;
 					OEN_w = 1'b0;
-					CEN_w = 1'b0;
-					WEN_w = 1'b1;
+					CEN_w = 1'b1;
+					WEN_w = 1'b0;
 					A_w = $unsigned( $signed(Freg_r[_fs]) + _immediate) >> 2;
 					Freg_w[_rt] = ReadDataMem;
 				end
 				4'd1: begin
 					process_counter_w = 4'd0;
 					OEN_w = 1'b0;
-					CEN_w = 1'b0;
-					WEN_w = 1'b1;
+					CEN_w = 1'b1;
+					WEN_w = 1'b0;
 					A_w = $unsigned( $signed(Freg_r[_fs+1]) + _immediate) >> 2;
 					Freg_w[_rt+1] = ReadDataMem;
 					IR_addr_w = IR_addr_r + 32'd4;
